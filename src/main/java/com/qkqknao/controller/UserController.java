4e94edd5e77d6e22958ed0f0f971ba787f37189e
HUIQUAN-PC\勋 <2327353506@qq.com>
2017-02-09 16:23:55 +0800

    mermaid

--- src/main/java/com/qkqknao/controller/UserController.java
+++ src/main/java/com/qkqknao/controller/UserController.java
@@ -2,10 +2,25 @@ package com.qkqknao.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping
+import com.qkqknao.util.ResponseMsg;
+/**
+ * @author wmt
+ * @date 2017-02-09
+ * @description 登录Controller
+ */
 @Controller
+@RequestMapping(value="/user")
 public class UserController {
+	
+
+	@RequestMapping(method=RequestMethod.POST)
+	@ResponseBody
+	public ResponseMsg login(){
+		return new ResponseMsg().setData("登录成功");
+	}
 
 	
 	
--- src/main/java/com/qkqknao/util/ResponseMsg.java
+++ src/main/java/com/qkqknao/util/ResponseMsg.java
@@ -1,5 +1,37 @@
 package com.qkqknao.util;
 
+/**
+ * @author wmt
+ * @date 2017-02-09
+ * @description Controller数据返回类
+ */
 public class ResponseMsg {
+	
+	private int code;
+	private String msg;
+	private Object data;
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Object getData() {
+		return data;
+	}
+	public ResponseMsg setData(Object data) {
+		this.data = data;
+		return this;
+	}
+	
+	
+	
 
 }
