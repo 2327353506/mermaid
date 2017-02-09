dcf864e3b473dac921521f7197c737a8fbd9c4ab
HUIQUAN-PC\勋 <2327353506@qq.com>
2017-02-09 15:09:30 +0800

    mermaid

---
+++ src/main/java/com/qkqknao/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.qkqknao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Controller
+public class UserController {
+
+	
+	
+}
