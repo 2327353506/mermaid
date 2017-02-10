9de5a2169b3c4a11403d651ee6c6a9cad6d32d63
HUIQUAN-PC\勋 <2327353506@qq.com>
2017-02-10 11:27:52 +0800

    mermaid

--- src/main/java/com/qkqknao/controller/UserController.java
+++ src/main/java/com/qkqknao/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.qkqknao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.qkqknao.service.UserServiceI;
 import com.qkqknao.util.ResponseMsg;
 /**
  * @author wmt
@@ -15,6 +17,8 @@ import com.qkqknao.util.ResponseMsg;
 @RequestMapping(value="/user")
 public class UserController {
 	
+	@Autowired
+	private UserServiceI userI;
 
 	@RequestMapping(method=RequestMethod.POST)
 	@ResponseBody
